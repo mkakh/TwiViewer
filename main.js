afba90b89d84af9d3b1842ae8477f7d45de89973
akh <akhs3214@gmail.com>
2017-11-05 19:43:36 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({frame: false, width: 800, height: 600, transparent: true})
+  mainWindow.loadURL('https://tweetdeck.twitter.com')
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
